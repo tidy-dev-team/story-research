94332f258f75b14b2dc1d6b50b97fe7433f4858d
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-06-04 16:26:21 +0300

    Fix box-sizing property in tailwindOverrideRules.css to ensure consistent layout

--- story/tailwind.config.js
+++ story/tailwind.config.js
@@ -1,46 +1,46 @@
 /** @type {import('tailwindcss').Config} */
-import plugin from 'tailwindcss/plugin'
-import { pzColors } from './src/ui-kit/foundations/colors'
-import { pzIconSizes, pzSpacing } from './src/ui-kit/foundations/spacings'
-import { pzSemanticColors } from './src/ui-kit/foundations/semanticColors'
-import { pzRoundings } from './src/ui-kit/foundations/roundings'
-import { pzTextStylesAsClassNames } from './src/ui-kit/foundations/textStyles'
-import { pzElevationsAsClassUtilities } from './src/ui-kit/foundations/elevations'
+import plugin from "tailwindcss/plugin";
+import { pzColors } from "./src/ui-kit/foundations/colors";
+import { pzIconSizes, pzSpacing } from "./src/ui-kit/foundations/spacings";
+import { pzSemanticColors } from "./src/ui-kit/foundations/semantic-colors";
+import { pzRoundings } from "./src/ui-kit/foundations/roundings";
+import { pzTextStylesAsClassNames } from "./src/ui-kit/foundations/text-styles";
+import { pzElevationsAsClassUtilities } from "./src/ui-kit/foundations/elevations";
 
 module.exports = {
-    content: ['../**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        // rounded-pz-3xs
-        borderRadius: {
-            pz: {
-                ...pzRoundings
-            }
-        },
-        colors: {
-            pz: {
-                ...pzColors,
-                ...pzSemanticColors
-            }
-        },
-        extend: {
-            spacing: {
-                pz: {
-                    ...pzSpacing,
-                    size: {
-                        icon: {
-                            ...pzIconSizes,
-                        }
-                    }
-                }
-            }
-        }
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // rounded-pz-3xs
+    borderRadius: {
+      pz: {
+        ...pzRoundings,
+      },
     },
-    plugins: [
-        plugin(function ({ addComponents }) {
-            addComponents(pzTextStylesAsClassNames)
-        }),
-        plugin(({ addUtilities }) => {
-            addUtilities(pzElevationsAsClassUtilities)
-        })
-    ]
-}
+    colors: {
+      pz: {
+        ...pzColors,
+        ...pzSemanticColors,
+      },
+    },
+    extend: {
+      spacing: {
+        pz: {
+          ...pzSpacing,
+          size: {
+            icon: {
+              ...pzIconSizes,
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents(pzTextStylesAsClassNames);
+    }),
+    plugin(({ addUtilities }) => {
+      addUtilities(pzElevationsAsClassUtilities);
+    }),
+  ],
+};
