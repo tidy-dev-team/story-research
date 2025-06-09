22dfd9909e715a7b5b7ea00132b04feaff6359ff
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-06-09 14:15:48 +0300

    avatar for review

---
+++ prisma-review/Avatar/Avatar.tsx
@@ -0,0 +1,66 @@
+import React, { ReactElement } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+export enum AvatarSize {
+  s = "s",
+  m = "m",
+  l = "l",
+}
+
+export enum AvatarType {
+  Default = "default",
+  Empty = "empty",
+}
+
+const avatarStyles = cva(
+  "flex justify-center items-center rounded-full select-none font-['Heebo',_sans-serif]",
+  {
+    variants: {
+      size: {
+        [AvatarSize.s]: "h-8 w-8 pz-body-s400",
+        [AvatarSize.m]: "h-14 w-14 pz-heading-m400",
+        [AvatarSize.l]: "h-24 w-24 pz-heading-xl500",
+      },
+      type: {
+        [AvatarType.Default]: "bg-pz-system-bg-primary text-pz-system-fg-black",
+        [AvatarType.Empty]: "bg-pz-system-bg-4 text-pz-system-fg-1",
+      },
+    },
+    defaultVariants: {
+      size: AvatarSize.m,
+      type: AvatarType.Default,
+    },
+  }
+);
+
+interface AvatarProps extends VariantProps<typeof avatarStyles> {
+  label?: string;
+  size?: AvatarSize;
+  type?: AvatarType;
+}
+
+const Avatar = ({
+  size = AvatarSize.m,
+  type,
+  label,
+}: AvatarProps): ReactElement => {
+  const processedLabel = label ? label.substring(0, 2) : label;
+
+  const avatarType =
+    type ??
+    (processedLabel && processedLabel.trim().length > 0
+      ? AvatarType.Default
+      : AvatarType.Empty);
+
+  const displayText =
+    avatarType === AvatarType.Default
+      ? processedLabel?.toUpperCase() || "AA"
+      : "N/A";
+
+  const classes = twMerge(avatarStyles({ size, type: avatarType }));
+
+  return <div className={classes}>{displayText}</div>;
+};
+
+export default Avatar;
