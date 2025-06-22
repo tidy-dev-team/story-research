7d42f08787afd16e6bdd0a72334c990214f6ebc6
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-06-22 11:59:37 +0300

    refactor: restructure button component semantic colors for clarity and consistency

--- story/src/stories/Button/buttonComponentSemanticColors.ts
+++ story/src/stories/Button/buttonComponentSemanticColors.ts
@@ -2,9 +2,19 @@ import { pzColors } from "../../ui-kit/foundations/colors";
 
 export const pzButtonComponentSemanticColors = {
   fg: {
+    primary: {
+      idle: pzColors.base.white,
+    },
     ghost: {
       idle: pzColors.gray[200],
+      hover: pzColors.base.white,
+      pressed: pzColors.gray[200],
     },
   },
-  bg: {},
+  bg: {
+    ghost: {
+      hover: pzColors.alpha.white["12p"],
+      pressed: pzColors.alpha.white["8p"],
+    },
+  },
 } as const;
