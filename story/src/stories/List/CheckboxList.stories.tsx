dbad7b00066942024802a690dcce9183ee74895c
Adir <slutzkis@gmail.com>
2025-07-02 10:37:05 +0300

    updates

---
+++ story/src/stories/List/CheckboxList.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { CheckboxList } from "./CheckboxList";
+import { TextDirection } from "../textDirection";
+
+const meta: Meta<typeof CheckboxList> = {
+    title: "Component/List/Checkbox List",
+    component: CheckboxList,
+    parameters: {
+        layout: "centered",
+    },
+    argTypes: {
+        items: {
+            control: { type: "object" },
+            description: "Array of checkbox labels",
+            defaultValue: ["Option 1", "Option 2", "Option 3"],
+        },
+        flow: {
+            control: { type: "radio" },
+            options: ["vertical", "horizontal"],
+        },
+        textDirection: {
+            control: { type: "radio" },
+            options: [TextDirection.Ltr, TextDirection.Rtl],
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CheckboxList>;
+
+export const Vertical: Story = {
+    args: {
+        items: ["Option 1", "Option 2", "Option 3"],
+        flow: "vertical",
+        textDirection: TextDirection.Ltr,
+    },
+};
+
+export const Horizontal: Story = {
+    args: {
+        items: ["Option 1", "Option 2", "Option 3"],
+        flow: "horizontal",
+        textDirection: TextDirection.Ltr,
+    },
+};
+
+export const RTL: Story = {
+    args: {
+        items: ["אפשרות 1", "אפשרות 2", "אפשרות 3"],
+        flow: "horizontal",
+        textDirection: TextDirection.Rtl,
+    },
+};
--- story/src/stories/List/RadioButtonList.stories.tsx
+++ story/src/stories/List/RadioButtonList.stories.tsx
@@ -1,12 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-import { CheckboxList } from "./CheckboxList";
 import { RadioButtonList } from "./RadioButtonList";
 import { TextDirection } from "../textDirection";
 
-const meta: Meta<typeof CheckboxList> = {
-    title: "Component/List/Checkbox List",
-    component: CheckboxList,
+const meta: Meta<typeof RadioButtonList> = {
+    title: "Component/List/RadioButton List",
+    component: RadioButtonList,
     parameters: {
         layout: "centered",
     },
@@ -29,7 +28,7 @@ const meta: Meta<typeof CheckboxList> = {
 
 export default meta;
 
-type Story = StoryObj<typeof CheckboxList>;
+type Story = StoryObj<typeof RadioButtonList>;
 
 export const Vertical: Story = {
     args: {
@@ -48,32 +47,6 @@ export const Horizontal: Story = {
 };
 
 export const RTL: Story = {
-    args: {
-        items: ["אפשרות 1", "אפשרות 2", "אפשרות 3"],
-        flow: "horizontal",
-        textDirection: TextDirection.Rtl,
-    },
-};
-
-type Story1 = StoryObj<typeof RadioButtonList>;
-
-export const VerticalRadio: Story1 = {
-    args: {
-        items: ["Option 1", "Option 2", "Option 3"],
-        flow: "vertical",
-        textDirection: TextDirection.Ltr,
-    },
-};
-
-export const HorizontalRadio: Story1 = {
-    args: {
-        items: ["Option 1", "Option 2", "Option 3"],
-        flow: "horizontal",
-        textDirection: TextDirection.Ltr,
-    },
-};
-
-export const RTLRadio: Story1 = {
     args: {
         items: ["אפשרות 1", "אפשרות 2", "אפשרות 3"],
         flow: "horizontal",
