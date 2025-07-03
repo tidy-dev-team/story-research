b6ee4c3e23ea98cbf9c58c6c0513fa608a600259
Adir <slutzkis@gmail.com>
2025-07-03 14:03:26 +0300

    updates

--- story/src/stories/List/List.stories.tsx
+++ story/src/stories/List/List.stories.tsx
@@ -4,6 +4,12 @@ import { TextDirection } from "../textDirection";
 import CloudQueueIcon from '@mui/icons-material/CloudQueue';
 import LanguageIcon from "@mui/icons-material/Language";
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import SignalWifiStatusbarNullIcon from '@mui/icons-material/SignalWifiStatusbarNull';
+import WysiwygIcon from '@mui/icons-material/Wysiwyg';
 
 const meta: Meta<typeof List> = {
     title: "Component/List/List",
@@ -40,6 +46,23 @@ export const RTL: Story = {
             { label: "בחירת שפה", icon: LanguageIcon },
             { label: "קבצי ענן", icon: CloudQueueIcon },
             { label: "שיחות", icon: ChatBubbleOutlineIcon },
+        ],
+    },
+};
+
+export const LongList: Story = {
+    args: {
+        textDirection: TextDirection.Ltr,
+        items: [
+            { label: "Language", icon: LanguageIcon },
+            { label: "Cloud Files", icon: CloudQueueIcon },
+            { label: "Chats", icon: ChatBubbleOutlineIcon },
+            { label: "Calendar", icon: CalendarTodayIcon },
+            { label: "Copy/ Paste", icon: ContentCopyIcon },
+            { label: "Error Management", icon: ErrorOutlineIcon },
+            { label: "Favorites", icon: StarOutlineIcon },
+            { label: "Networks", icon: SignalWifiStatusbarNullIcon },
+            { label: "Interfaces", icon: WysiwygIcon },
         ],
     },
 };
