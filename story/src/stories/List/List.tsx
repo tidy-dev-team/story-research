394061f24e2a28dbc8c62b6fe218f46b68b08587
Adir <slutzkis@gmail.com>
2025-07-03 13:55:50 +0300

    Update List.tsx

--- story/src/stories/List/List.tsx
+++ story/src/stories/List/List.tsx
@@ -18,7 +18,7 @@ const ListStyles = cva([
 ]);
 
 const ListItemStyles = cva([
-    "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "text-pz-gray-100", "px-pz-3xs", "py-pz-5xs", "min-h-pz-l", "rounded-pz-xs", "cursor-pointer", "hover:bg-pz-gray-950", "hover:after:absolute", "hover:after:bg-pz-system-bg-overlay-hover", "hover:after:inset-0", "active:after:bg-pz-system-bg-overlay-pressed", "border", "border-transparent", "border-2", "outline-none", "focus:border-pz-system-border-focused-1"
+    "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "text-pz-gray-100", "px-pz-3xs", "py-pz-5xs", "min-h-pz-l", "rounded-pz-xs", "border", "border-transparent", "border-2", "outline-none", "cursor-pointer", "hover:bg-pz-gray-950", "hover:after:absolute", "hover:after:bg-pz-system-bg-overlay-hover", "hover:after:inset-0", "active:after:bg-pz-system-bg-overlay-pressed", "focus:border-pz-system-border-focused-1"
 ])
 
 export const List = ({
