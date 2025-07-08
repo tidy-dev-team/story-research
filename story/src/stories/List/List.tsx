811de0825ad1fd2a768415fb30a47b754e7277b5
Adir <slutzkis@gmail.com>
2025-07-08 15:46:15 +0300

    Update List.tsx

--- story/src/stories/List/List.tsx
+++ story/src/stories/List/List.tsx
@@ -13,11 +13,11 @@ export interface ListProps {
     textDirection?: TextDirection;
 }
 
-const ListStyles = cva([
+const listStyles = cva([
     "flex", "flex-col", "gap-pz-3xs", "pz-label-l", "w-[268px]",
 ]);
 
-const ListItemStyles = cva([
+const listItemStyles = cva([
     "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "text-pz-gray-100", "px-pz-3xs", "py-pz-5xs", "min-h-pz-l", "rounded-pz-xs", "border-transparent", "border-2", "outline-none", "cursor-pointer", "hover:bg-pz-gray-950", "hover:after:absolute", "hover:after:bg-pz-system-bg-overlay-hover", "hover:after:inset-0", "active:after:bg-pz-system-bg-overlay-pressed", "focus-visible:border-pz-system-border-focused-1"
 ])
 
@@ -26,12 +26,12 @@ export const List = ({
     textDirection = TextDirection.Ltr,
 }: ListProps) => {
     return (
-        <ul className={ListStyles()} dir={textDirection}>
+        <ul className={listStyles()} dir={textDirection}>
             {items.map(({ label, icon: Icon }, idx) => (
                 <li
                     key={idx}
                     tabIndex={0}
-                    className={ListItemStyles()}
+                    className={listItemStyles()}
                 >
                     <p>{label}</p>
                     {Icon && <Icon color="inherit" fontSize="inherit" />}
