ac7e3038fe4dc9f1cc6508770d578f0bcb557370
Adir <42638312+Adir-SL@users.noreply.github.com>
2025-07-08 17:11:09 +0300

    Update List.tsx

--- prisma-review/List.tsx
+++ prisma-review/List.tsx
@@ -13,12 +13,12 @@ export interface ListProps {
     textDirection?: TextDirection;
 }
 
-const ListStyles = cva([
-    "flex", "flex-col", "gap-pz-2xs", "pz-label-l", "w-[268px]",
+const listStyles = cva([
+    "flex", "flex-col", "gap-pz-3xs", "pz-label-l", "w-[268px]",
 ]);
 
-const ListItemStyles = cva([
-    "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "text-pz-gray-100", "px-pz-3xs", "py-pz-5xs", "min-h-pz-l", "rounded-pz-xs", "border", "border-transparent", "border-2", "outline-none", "cursor-pointer", "hover:bg-pz-gray-950", "hover:after:absolute", "hover:after:bg-pz-system-bg-overlay-hover", "hover:after:inset-0", "active:after:bg-pz-system-bg-overlay-pressed", "focus-visible:border-pz-system-border-focused-1"
+const listItemStyles = cva([
+    "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "text-pz-gray-100", "px-pz-3xs", "py-pz-5xs", "min-h-pz-l", "rounded-pz-xs", "border-transparent", "border-2", "outline-none", "cursor-pointer", "hover:bg-pz-gray-950", "hover:after:absolute", "hover:after:bg-pz-system-bg-overlay-hover", "hover:after:inset-0", "active:after:bg-pz-system-bg-overlay-pressed", "focus-visible:border-pz-system-border-focused-1"
 ])
 
 export const List = ({
@@ -26,14 +26,14 @@ export const List = ({
     textDirection = TextDirection.Ltr,
 }: ListProps) => {
     return (
-        <ul className={ListStyles()} dir={textDirection}>
+        <ul className={listStyles()} dir={textDirection}>
             {items.map(({ label, icon: Icon }, idx) => (
                 <li
                     key={idx}
                     tabIndex={0}
-                    className={ListItemStyles()}
+                    className={listItemStyles()}
                 >
-                    <p>{label}</p>
+                    <span>{label}</span>
                     {Icon && <Icon color="inherit" fontSize="inherit" />}
                 </li>
             ))}
