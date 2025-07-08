9906560db05dd23059b131d325c1ab1bb4005f74
Adir <42638312+Adir-SL@users.noreply.github.com>
2025-07-08 17:10:29 +0300

    Update ListCard.tsx

--- prisma-review/ListCard.tsx
+++ prisma-review/ListCard.tsx
@@ -3,7 +3,8 @@ import { cva } from "class-variance-authority";
 import { TextDirection } from "../textDirection";
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import { IconButton, ButtonSize, ButtonType } from "../Button/IconButton";
+import { IconButton, IconButtonType } from "../Button/IconButton";
+import { Ghost } from "../Button/IconButton.stories";
 
 export interface ListItem {
     label: string;
@@ -14,12 +15,12 @@ export interface ListProps {
     textDirection?: TextDirection;
 }
 
-const ListStyles = cva([
-    "flex", "flex-col", "gap-pz-2xs", "pz-label-m", "w-[268px]",
+const listStyles = cva([
+    "flex", "flex-col", "gap-pz-3xs", "pz-label-m", "w-[268px]",
 ]);
 
-const ListItemStyles = cva([
-    "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "text-pz-gray-100", "px-pz-3xs", "py-pz-4xs", "rounded-pz-xs", "border", "border-transparent", "border-2", "outline-none", "cursor-pointer", "hover:bg-pz-gray-950", "hover:before:absolute", "hover:before:bg-pz-system-bg-overlay-hover", "hover:before:inset-0", "hover:before:pointer-events-none", "active:before:bg-pz-system-bg-overlay-pressed", "focus-visible:border-pz-system-border-focused-1"
+const listItemStyles = cva([
+    "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "bg-pz-gray-950", "text-pz-gray-100", "px-pz-3xs", "py-pz-4xs", "rounded-pz-xs", "border", "border-transparent", "border-2", "outline-none", "cursor-pointer", "hover:bg-pz-gray-950", "hover:before:absolute", "hover:before:bg-pz-system-bg-overlay-hover", "hover:before:inset-0", "hover:before:pointer-events-none", "active:before:bg-pz-system-bg-overlay-pressed", "focus-visible:border-pz-system-border-focused-1"
 ])
 
 export const ListCard = ({
@@ -27,17 +28,25 @@ export const ListCard = ({
     textDirection = TextDirection.Ltr,
 }: ListProps) => {
     return (
-        <ul className={ListStyles()} dir={textDirection}>
+        <ul className={listStyles()} dir={textDirection}>
             {items.map(({ label }, idx) => (
                 <li
                     key={idx}
                     tabIndex={0}
-                    className={ListItemStyles()}
+                    className={listItemStyles()}
                 >
                     <p>{label}</p>
                     <span className="flex gap-pz-4xs">
-                        <IconButton size={ButtonSize.Medium} type={ButtonType.Ghost} icon={<EditOutlinedIcon />} />
-                        <IconButton size={ButtonSize.Medium} type={ButtonType.Ghost} icon={<DeleteOutlinedIcon />} />
+                        <IconButton
+                            icon={EditOutlinedIcon}
+                            onClick={() => console.log(`Edit ${label}`)}
+                            type={IconButtonType.Ghost}
+                        />
+                        <IconButton
+                            icon={DeleteOutlinedIcon}
+                            onClick={() => console.log(`Delete ${label}`)}
+                            type={IconButtonType.Ghost}
+                        />
                     </span>
                 </li>
             ))}
