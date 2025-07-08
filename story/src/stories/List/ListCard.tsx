4a3d9567aecdae74a2b82413d4a2373feb10803a
Adir <slutzkis@gmail.com>
2025-07-08 15:53:03 +0300

    Update ListCard.tsx

--- story/src/stories/List/ListCard.tsx
+++ story/src/stories/List/ListCard.tsx
@@ -15,11 +15,11 @@ export interface ListProps {
     textDirection?: TextDirection;
 }
 
-const ListStyles = cva([
+const listStyles = cva([
     "flex", "flex-col", "gap-pz-3xs", "pz-label-m", "w-[268px]",
 ]);
 
-const ListItemStyles = cva([
+const listItemStyles = cva([
     "flex", "relative", "justify-between", "items-center", "overflow-hidden", "gap-2", "bg-pz-gray-950", "text-pz-gray-100", "px-pz-3xs", "py-pz-4xs", "rounded-pz-xs", "border", "border-transparent", "border-2", "outline-none", "cursor-pointer", "hover:bg-pz-gray-950", "hover:before:absolute", "hover:before:bg-pz-system-bg-overlay-hover", "hover:before:inset-0", "hover:before:pointer-events-none", "active:before:bg-pz-system-bg-overlay-pressed", "focus-visible:border-pz-system-border-focused-1"
 ])
 
@@ -28,12 +28,12 @@ export const ListCard = ({
     textDirection = TextDirection.Ltr,
 }: ListProps) => {
     return (
-        <ul className={ListStyles()} dir={textDirection}>
+        <ul className={listStyles()} dir={textDirection}>
             {items.map(({ label }, idx) => (
                 <li
                     key={idx}
                     tabIndex={0}
-                    className={ListItemStyles()}
+                    className={listItemStyles()}
                 >
                     <p>{label}</p>
                     <span className="flex gap-pz-4xs">
