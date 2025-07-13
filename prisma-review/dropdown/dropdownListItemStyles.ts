fa3d8e7613798af2909132092561bee86f1ea3b3
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-07-13 15:52:08 +0300

    dropdown items for review

--- prisma-review/dropdown/dropdownListItemStyles.ts
+++ prisma-review/dropdown/dropdownListItemStyles.ts
@@ -1,8 +1,18 @@
-import { cva } from "class-variance-authority";
+import { cva, VariantProps } from "class-variance-authority";
+
+export enum DropdownListItemVariant {
+  Text = "text",
+  Severity = "severity",
+}
+
+export enum DropdownListItemPaddingVariant {
+  Simple = "simple",
+  Complex = "complex",
+}
 
 export const baseDropdownListItemStyles = [
   "flex items-center w-full box-border overflow-hidden",
-  "border-none bg-transparent cursor-pointer",
+  "border-none bg-transparent",
   "text-pz-system-fg-1 pz-label-m",
   "rounded-pz-2xs",
   "transition-all duration-200",
@@ -12,39 +22,29 @@ export const baseDropdownListItemStyles = [
   "focus-visible:ring-pz-system-border-focused-1",
 ];
 
-export const dropdownListItemPaddingVariants = {
-  simple: "px-pz-4xs py-pz-3xs gap-pz-4xs h-8",
-  complex: "p-pz-4xs gap-pz-4xs min-h-8",
-};
-
-export const dropdownListItemFocusVariants = {
-  simple: "",
-  enhanced:
-    "focus-visible:rounded-pz-2xs ring-offset-1 ring-offset-pz-gray-1000",
-};
-
-export const dropdownListItemDisabledVariants = {
-  disabled: {
-    true: "text-pz-system-fg-disabled cursor-not-allowed hover:bg-transparent",
-    false: "",
+export const getDropdownListStyles = cva(baseDropdownListItemStyles, {
+  variants: {
+    isDisabled: {
+      true: "text-pz-system-fg-disabled cursor-not-allowed hover:bg-transparent",
+      false: "cursor-pointer",
+    },
+    isFocused: {
+      true: "focus-visible:rounded-pz-2xs ring-offset-1 ring-offset-pz-gray-1000",
+      false: "",
+    },
+    paddingVariant: {
+      [DropdownListItemPaddingVariant.Simple]:
+        "px-pz-4xs py-pz-3xs gap-pz-4xs h-8",
+      [DropdownListItemPaddingVariant.Complex]: "p-pz-4xs gap-pz-4xs min-h-8",
+    },
   },
-};
+  defaultVariants: {
+    isDisabled: false,
+    isFocused: false,
+    paddingVariant: DropdownListItemPaddingVariant.Simple,
+  },
+});
 
-export const createDropdownListItemStyles = (
-  paddingVariant: keyof typeof dropdownListItemPaddingVariants = "simple",
-  focusVariant: keyof typeof dropdownListItemFocusVariants = "simple"
-) => {
-  return cva(
-    [
-      ...baseDropdownListItemStyles,
-      dropdownListItemPaddingVariants[paddingVariant],
-      dropdownListItemFocusVariants[focusVariant],
-    ],
-    {
-      variants: dropdownListItemDisabledVariants,
-      defaultVariants: {
-        disabled: false,
-      },
-    }
-  );
-};
+export type GetDropdownListStylesProps = VariantProps<
+  typeof getDropdownListStyles
+>;
